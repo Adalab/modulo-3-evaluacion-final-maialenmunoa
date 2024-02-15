33a7d1b6dd45641f017fde2b598c6fb1b3aa8a96
maialenmunoa <maialenmunoa@gmail.com>
2024-02-15 18:58:27 +0100

    fixed view problem

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 function Header() {
-    return (
-        <header>Personajes de Harry Potter
-        <img src="" alt="" />
-        </header>
-    )
-  }
-  
-export default Header
+  return (
+    <header>
+      Personajes de Harry Potter
+      <img src="" alt="" />
+    </header>
+  );
+}
+
+export default Header;
