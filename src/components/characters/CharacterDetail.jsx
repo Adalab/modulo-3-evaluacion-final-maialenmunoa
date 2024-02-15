ffbb323d10caa8f4704118e4b0d0c79148dbfef1
maialenmunoa <maialenmunoa@gmail.com>
2024-02-15 20:17:23 +0100

    add character detail html

--- src/components/characters/CharacterDetail.jsx
+++ src/components/characters/CharacterDetail.jsx
@@ -1,9 +1,21 @@
 function CharacterDetail() {
+    const character = character
+
     return (
-      <div>
-  
-      </div>
+      <>
+        <img 
+        src={character.image ||
+            "https://via.placeholder.com/210x295/%C8%C8%C8/666666/?text=HarryPotter"} 
+            alt={character.name} />
+        <div className="character-info">
+          <h1>{character.name}</h1>
+          <p>{character.alive ? 'Vivo' : 'Muerto'}</p>
+          <p>{character.species}</p>
+          <p>{character.gender}</p>
+          <p>{character.house}</p>
+        </div>
+      </>
     )
   }
   
-  export default CharacterDetail
+  export default CharacterDetail;
