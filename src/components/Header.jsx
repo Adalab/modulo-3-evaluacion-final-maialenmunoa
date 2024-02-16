6b46c9f022c7d417956ef0afa0b582e50fe630ef
maialenmunoa <maialenmunoa@gmail.com>
2024-02-16 16:54:00 +0100

    add logo and shield in footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
 
+import HogwartsLogo from '../images/harry_potter_hogwarts_logo.png';
+import HouseShield from '../images/harry_potter_escudo.png';
+
+import '../scss/Header.scss';
+
+
 function Header() {
   return (
     <Link to="/personajes">
-    <header>
-        Personajes de Harry Potter
-        <img src="" alt="" />
+      <header className="header">
+          <img className="header__shield" src={HouseShield} alt="Escudo Harry Potter" />
+          <img className="header__logo" src={HogwartsLogo} alt="Logo Hogwarts de Harry Potter" />
       </header>
     </Link>
   );
