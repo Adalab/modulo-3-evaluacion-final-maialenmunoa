10472a4ba6925d2bd777cacbea0c52289179ba79
maialenmunoa <maialenmunoa@gmail.com>
2024-02-16 16:34:43 +0100

    add changes

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function HomePage() {
     return (
       <div>
-        <h1>Spectro Patronum</h1>
+        <h1>Expecto Patronum</h1>
         <p>Descubre todo lo que necesitas saber sobre los personajes de Harry Potter</p>
         <Link to="/personajes">ENTRAR{" "}</Link>
       </div>
