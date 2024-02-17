22583b630221ff775c0c1732db1a20f7947e28ad
maialenmunoa <maialenmunoa@gmail.com>
2024-02-17 12:23:36 +0100

    add some scss animation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 function Footer() {
     return (
         <footer>
+        ¿Después de todo este tiempo? Siempre
         <p>Harry Potter &copy; 2021</p>
         </footer>
     )
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -9,10 +9,11 @@ function HomePage() {
       <div className="home">
         <div className="home__content">
         <img className="home__content-logo" src={HarryLogo} alt="Harry Potter" />
-        <h1 className="home__content-title">Welcome to the world of Harry Potter!</h1>
+        <h1 className="home__content-title">Descubre los personajes de Harry Potter</h1>
         <p className="home__content-text">Expecto Patronum</p>
+        <p className="home__content-typewriter">Draco dormiens nunquam titillandus.</p>
         <Link className="home__content-link" to="/personajes">
-          <button className="home__content-button">ENTER{" "}</button>
+          <button className="home__content-button">ENTRAR{" "}</button>
         </Link>
         </div>
       </div>
