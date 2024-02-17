0771de1334086a5e85b00a2882faeb2b939f3fbb
maialenmunoa <maialenmunoa@gmail.com>
2024-02-17 12:32:30 +0100

    add footer styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,17 @@
+import github from '../images/github-icon.png'
+
+import '../scss/Footer.scss';
+
 function Footer() {
     return (
-        <footer>
-        ¿Después de todo este tiempo? Siempre
-        <p>Harry Potter &copy; 2021</p>
-        </footer>
+        <footer className="footer">
+            <p className="footer__text">¿Después de todo este tiempo? Siempre...</p>
+            <p className="footer__text-author">Hecho con <span role="img" aria-label="corazón">❤️</span> por Maialen</p>
+            <div className="footer__link">
+                <img className="footer__logo" src={github} alt="GitHub Logo" />
+                <a className= "footer__logo-link" href="https://github.com/Adalab/modulo-3-evaluacion-intermedia-maialenmunoa" target="_blank" rel="noreferrer">Repositorio</a>
+            </div>
+       </footer>
     )
   }
   
