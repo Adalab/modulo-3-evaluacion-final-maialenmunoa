129302607b45880c2f5806c566cd1693632ce001
maialenmunoa <maialenmunoa@gmail.com>
2024-02-17 11:09:27 +0100

    add CharacterDetail styles

--- src/components/characters/CharacterDetail.jsx
+++ src/components/characters/CharacterDetail.jsx
@@ -1,6 +1,8 @@
 import PropTypes from "prop-types";
 import {  Link, useParams } from "react-router-dom";
 
+import '../../scss/Detail.scss';
+
 function CharacterDetail( {findCharacter}) {
 
   const params = useParams();
@@ -8,20 +10,28 @@ function CharacterDetail( {findCharacter}) {
   const character = findCharacter(params.id)
 
     return (
-      <>
-        <img 
-        src={character.image ||
+      <div className="detail">
+        <div className="detail__content">
+          <img className="detail__content-img"
+          src={character.image ||
             "https://via.placeholder.com/210x295/%C8%C8%C8/666666/?text=HarryPotter"} 
             alt={character.name} />
-        <div className="character-info">
-          <h1>{character.name}</h1>
-          <p>{character.alive ? 'Vivo' : 'Muerto'}</p>
-          <p>{character.species}</p>
-          <p>{character.gender}</p>
-          <p>{character.house}</p>
-          <Link to="/personajes">Volver</Link>
+            <div className="detail__content-info">
+              <h1 className="detail__content-info-name">{character.name}</h1>
+              <p className="detail__content-info-icon">{character.alive ? (
+                <span className="alive-icon"><i className="fas fa-heart"></i></span>
+              ) : (
+                <span className="dead-icon"><i className="fas fa-skull"></i></span>
+              )}</p>
+              <p>{character.species}</p>
+              <p>{character.gender}</p>
+              <p>{character.house}</p>
+              <Link to="/personajes">
+                <button className="detail__content-info-button">VOLVER</button>
+              </Link>
+            </div>
         </div>
-      </>
+      </div>
     )
   }
 
