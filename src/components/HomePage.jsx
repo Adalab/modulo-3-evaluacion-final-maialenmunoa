c4dfa133e6e9eeceda2194381b0bdf3535b02903
maialenmunoa <maialenmunoa@gmail.com>
2024-02-19 11:37:27 +0100

    add fonts

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -10,8 +10,7 @@ function HomePage() {
         <div className="home__content">
         <img className="home__content-logo" src={HarryLogo} alt="Harry Potter" />
         <h1 className="home__content-title">Descubre los personajes de Harry Potter</h1>
-        <p className="home__content-text">Expecto Patronum</p>
-        <p className="home__content-typewriter">Draco dormiens nunquam titillandus.</p>
+        <p className="home__content-text">Draco dormiens nunquam titillandus</p>
         <Link className="home__content-link" to="/personajes">
           <button className="home__content-button">ENTRAR{" "}</button>
         </Link>
